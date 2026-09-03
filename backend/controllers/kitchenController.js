1b481cbe49b17c7802213ebc71d0a7f29d3b3498
mandela604 <mandelaidiahi@gmail.com>
2026-09-03 00:18:20 -0700

    fix: stop overriding department price/cost on delivery receipt

--- backend/controllers/kitchenController.js
+++ backend/controllers/kitchenController.js
@@ -564,7 +564,6 @@ exports.receiveRequisition = asyncHandler(async (req, res) => {
 
     if (stockItem) {
       stockItem.qty += addQty;
-      if (Number(it.cost) > 0) { stockItem.price = Number(it.cost); stockItem.cost = Number(it.cost); }
       await stockItem.save();
     } else {
       stockItem = await KitchenStock.create({
